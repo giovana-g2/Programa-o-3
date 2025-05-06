702f06a6d4f2d46ee2cb588c61ba85c909ef24e3
Giovana Gamarra <giovana.2023302580@aluno.iffar.edu.br>
2025-05-06 10:49:33 -0300

    Update ex1.php

--- aula 06.05/ex1.php
+++ aula 06.05/ex1.php
@@ -22,6 +22,15 @@ $p2->nome = "caneta azul";
 $p2->preco = 4.5;
 $p2->quantidade = 10;
 $p2->exibirInformaçoes();
+
+function mostrarMediaPrecos ($p1, $p2){
+    $preco1 = str_replace(['R$', ','], ['', '.'], $p1->preco);
+    $preco2 = str_replace(['R$', ','], ['', '.'], $p2->preco);
+    
+    $media = ($preco1 + $preco2) / 2;
+    echo "<br>A média dos preços dos produtos é: R$".number_format($media, 2, ',', '.')."<br>";
+    }
+    mostrarMediaPrecos($p1, $p2);
     
 
 
